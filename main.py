188ca0e46f00bed34b4d73ae6783a0f12abe9211
mannievilla <mannievilla@gmail.com>
2022-04-16 14:57:56 -0500

    saving th main.py total death and cases function for leaftlet

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 #first thing you need for flask
 
-from flask import Flask, jsonify, render_template, redirect
+from flask import Flask, jsonify, render_template, redirect, request
 import sqlite3
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
 import json
+
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine, inspect, func
+import datetime
+
 
 
 #dEcribe where the application is being run from
@@ -27,6 +32,28 @@ def index():
 def about():
     return "Welcome to the About page!"
 
+
+
+@app.route('/getTotalDeathByAllState', methods=['GET'])
+def getTotalDeathByAlltate():
+    database_path = "Data/COVID_Data.db"
+
+    engine = create_engine(f"sqlite:///{database_path}")
+
+    cases_data = engine.execute("SELECT tot_cases, state FROM (SELECT tot_cases, state, ROW_NUMBER() OVER(PARTITION BY state ORDER BY submission_date DESC) as row_num FROM usa_data) table1 WHERE row_num = 1;")
+
+    deaths_data = engine.execute("SELECT tot_death, state FROM (SELECT tot_death, state, ROW_NUMBER() OVER(PARTITION BY state ORDER BY submission_date DESC) as row_num FROM usa_data) table1 WHERE row_num = 1;")
+
+    return jsonify(
+        {
+            'status' : 'success',
+            'results' :{
+                'cases' : cases_data,
+                'deaths' : deaths_data
+            }
+        })
+
+
 #last piece we need, useful of launching from gitbash it will be name but if running from cloud service it may be "main"
 if __name__ == "__main__":
     app.run(debug=True)
@@ -43,3 +70,24 @@ if __name__ == "__main__":
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
+
+
+
+# Dependencies and Setup
+
+
+# File to Load (Remember to Change These)
+
+database_path = "Data/COVID_Data.db"
+
+
+engine = create_engine(f"sqlite:///{database_path}")
+
+
+
+cases_data = engine.execute("SELECT tot_cases, state FROM (SELECT tot_cases, state, ROW_NUMBER() OVER(PARTITION BY state ORDER BY submission_date DESC) as row_num FROM usa_data) table1 WHERE row_num = 1;")
+
+
+deaths_data = engine.execute("SELECT tot_death, state FROM (SELECT tot_death, state, ROW_NUMBER() OVER(PARTITION BY state ORDER BY submission_date DESC) as row_num FROM usa_data) table1 WHERE row_num = 1;")
+
+
