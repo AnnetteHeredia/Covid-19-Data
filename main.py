83aaa499842ebad478fb9916b49d006954349610
mannievilla <mannievilla@gmail.com>
2022-04-16 14:11:43 -0500

    up tp date on main.py

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
-from flask import Flask, render_template
-import flask
-app = Flask(__name__)
-@app.route('/')
-def home():
-    print(flask.current_app.root_path)
-    return "Hello"
-    # return render_template('Project\ 3\ Home/index.html')
+#first thing you need for flask
 
+from flask import Flask, jsonify, render_template, redirect
+import sqlite3
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import json
+
+
+#dEcribe where the application is being run from
+app = Flask(__name__)
+
+
+#trick for annotation a finctiionality to  in Python add  @  it tells it the route "/"
+@app.route("/")
+
+#simple function that returns this statement
+def index():
+    return "Welcome to Get Leaf!"
+
+#telling the route and that it need to run this function
+@app.route("/about")
+
+#simple function that returns this statement
+def about():
+    return "Welcome to the About page!"
+
+#last piece we need, useful of launching from gitbash it will be name but if running from cloud service it may be "main"
 if __name__ == "__main__":
     app.run(debug=True)
-    
+
+
+
+
+# the basic boiler plate shell for app.py
+# from flask import Flask, jsonify
+
+# app = Flask(__name__)
+
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
---
+++ usa_logic.js
@@ -0,0 +1,92 @@
+
+
+d3.json('state_lat_long.geojson').then((data) => {
+  // console.log(data);
+  var features = data.features
+  console.log(features)
+  // var latitude = data.features[0]
+  //creating a path to get to coordinates. we use forEach() funciton to do this and call it location
+  // features.forEach((feature) => {
+  //     state = feature.properties.State
+  //     location
+  //     console.log(state)
+  // })
+
+  for (var i = 0; i < features.length; i++) {
+    var feature = features[i];
+    var myCoordinates = [feature.geometry.coordinates[1],feature.geometry.coordinates[0]]
+    console.log(myCoordinates)
+    L.marker(myCoordinates)
+      .bindPopup(`<h1>${feature.properties.State}</h1>`) //<hr> <h3>Population ${city.population.toLocaleString()}</h3>`)
+      .addTo(myMap);
+  }
+  // fetch('United_States_COVID-19_Cases_and_Deaths_by_State_over_Time (1).csv').then(function(response){
+  //   //console.log(response) //just a promise
+  //     return response.text();
+  // }).then(function(text){
+  //     //console.log(text)
+  //     var lines = text.split("\n");
+  //     for (var a = 1; a < lines.length; a++) {
+  //     var line = lines[a];
+  //     var parts = line.split(",");
+  //       for (var l = 0; l < parts.length; l++) {
+  //        var part =parts[l]
+  //         console.log(part)
+  //       }
+  //     }
+  //     // Handling of the text contents goes here
+  // }).catch(function(err){
+  //     // Error handling goes here (e.g. the network request failed, etc)
+  // })
+  
+  //testfunction(data)
+}) 
+
+
+
+// function testfunction(testdata) {
+//   console.log("I am in test function")
+
+//   console.log(testdata)
+// }
+
+
+// getLong = d3.json('state_lat_long.geojson').then((data) => {
+//   // console.log(data);
+//   var features = data.features
+//   console.log(features)
+//   // var latitude = data.features[0]
+//   //creating a path to get to coordinates. we use forEach() funciton to do this and call it location
+//   features.forEach((location) => {
+//       longitude = location.geometry.coordinates[0]
+//       // console.log(longitude)
+//   })
+// })
+// console.log(getLong)
+
+
+
+// getLat = d3.json('state_lat_long.geojson').then((data) => {
+//   // console.log(data);
+//   var features = data.features
+//   console.log(features)
+//   // var latitude = data.features[0]
+//   //creating a path to get to coordinates. we use forEach() funciton to do this and call it location
+//   features.forEach((location) => {
+//       longitude = location.geometry.coordinates[1]
+//       // console.log(longitude)
+//   })
+// })
+// console.log(getLat)
+
+// Create a map object.
+var myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 5
+});
+
+
+// Add a tile layer.
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
